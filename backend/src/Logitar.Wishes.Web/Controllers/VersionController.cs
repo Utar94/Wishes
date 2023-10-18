e127b96acc439e9dfbc0064b6005201e5ed97f35
Francis Pion <francispion@hotmail.com>
2023-10-18 12:24:38 -0400

    Added HomeController and VersionController. (#19)

---
+++ backend/src/Logitar.Wishes.Web/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using Logitar.Wishes.Web.Models.Version;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Wishes.Web.Controllers;
+
+[ApiController]
+[Route("version")]
+public class VersionController : ControllerBase
+{
+  private readonly Version _version;
+
+  public VersionController(IConfiguration configuration)
+  {
+    _version = new Version(configuration.GetValue<string>("Version") ?? string.Empty);
+  }
+
+  [HttpGet]
+  public ActionResult<ApplicationVersion> Get() => Ok(new ApplicationVersion(_version));
+}
---
+++ backend/src/Logitar.Wishes.Web/Models/Version/ApplicationVersion.cs
@@ -0,0 +1,11 @@
+﻿namespace Logitar.Wishes.Web.Models.Version;
+
+public record ApplicationVersion
+{
+  public string Version { get; }
+
+  public ApplicationVersion(System.Version version)
+  {
+    Version = version.ToString();
+  }
+}
