7f771eb9a401d85ada68fa8a92958520ff191c93
Francis Pion <francispion@hotmail.com>
2023-10-19 13:34:24 -0400

    Reduced security on ProtectedCache. (#28)

--- backend/src/Logitar.Wishes.Infrastructure/Caching/ProtectedCache.cs
+++ backend/src/Logitar.Wishes.Infrastructure/Caching/ProtectedCache.cs
@@ -10,7 +10,11 @@ internal record ProtectedCache<T>
   public ProtectedCache(T value, string password)
   {
     _value = value;
-    _pbkdf2 = new(password);
+
+    /* NOTE(fpion): OWASP recommends 600000 iterations when using SHA-256. This is a security risk
+     * since the Portal uses 600000 iterations. The result is that Wishes is less secure than the
+     * Portal, which simplifies the job of an attack who would like to try keys. */
+    _pbkdf2 = new(password, iterations: 10000);
   }
 
   public T? GetValue(string password) => _pbkdf2.IsMatch(password) ? _value : default;
